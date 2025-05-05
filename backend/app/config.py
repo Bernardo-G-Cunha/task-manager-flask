8f11cb6a459b7bf3760ef8b6dca874375ce5d4c9
Bernardo-G-Cunha <bernardo.garcia.cunha@gmail.com>
2025-05-05 11:24:41 -0300

    Creates general structure.

---
+++ backend/app/config.py
@@ -0,0 +1,11 @@
+import os
+from datetime import timedelta
+
+class Config:
+    SECRET_KEY= os.environ.get('SECRET_KEY', 'dev-secret')
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3')
+
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt-secret')
+
+    JWT_ACESS_TOKEN_EXPIRES = timedelta(hours=1)
---
+++ backend/app/routes/auth.py
@@ -0,0 +1,34 @@
+import os
+from flask import Blueprint, request, make_response, render_template, redirect, url_for, Response, jsonify, session
+
+auth = Blueprint('auth', __name__, template_folder='templates')
+
+
+@auth.route('/')
+def index():
+    return render_template('index.html')
+
+
+@auth.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    
+    # Pegar valor no db
+    isUsername = True
+    isPassword = True
+    isUser = (isUsername and isPassword)
+
+    if isUser == True:
+        session['username'] = username
+        return redirect(url_for('tasks'))
+    else:
+        if not isUser:
+            response = jsonify({'Error': 'Invalid User'})
+            return response, 404 
+        else:
+            response = jsonify({'Error': 'Wrong Password'})
+            return response, 401
+           
+
+
