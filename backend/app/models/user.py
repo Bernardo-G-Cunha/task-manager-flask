32643149aa7cb9eeef1d3c5a5ddad3da569fed66
Bernardo-G-Cunha <bernardo.garcia.cunha@gmail.com>
2025-05-08 22:29:02 -0300

    Corrects models and adds basic methods to them.

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -9,5 +9,16 @@ class User(db.Model):
     email = db.Column(db.String, nullable = False)
     
     tasks = db.relationship('Task', back_populate='user')
-    
     tags = db.relationship('Tag', secondary=tasks_tags, back_populates='users')
+
+    def __repr__(self):
+        return f'<User id={self.id} name={self.name} email={self.email}>'
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
