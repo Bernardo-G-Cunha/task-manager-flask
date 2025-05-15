315c9048a1e627a08f5fd8141140f625bb9e7f14
Bernardo-G-Cunha <bernardo.garcia.cunha@gmail.com>
2025-05-15 20:10:42 -0300

    Finishes login endpoint and puts part of the logic in the directory services.

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,10 +1,13 @@
-import os
-from flask import Blueprint, request, make_response, render_template, redirect, url_for, Response, jsonify, session
-from sqlalchemy.exc import NoResultFound, MultipleResultsFound
+from flask import Blueprint, request, jsonify
+from sqlalchemy.orm.exc import NoResultFound
+from flask_jwt_extended import create_access_token
+from marshmallow import ValidationError
 from app.extensions import bcrypt
 from app.schemas.user_schema import UserSchema
 from app.models.user import User
-from marshmallow import ValidationError
+from app.services.auth import verify_user
+
+#------------------------------------------------------------------------------------------------------------------
 
 auth_bp = Blueprint('auth', __name__, template_folder='templates')
 
@@ -12,21 +15,21 @@ user_schema = UserSchema()
 
 @auth_bp.route('/', methods=['POST'])
 def login():
+    #Gets request's data
     try:
         data = user_schema.load(request.get_json())
-        email = data['email']
-        password = data['password']
+        user = verify_user(data['email'], data['password'])
 
     except ValidationError as err:
         return jsonify({"error": err.messsages}), 400
+    
+    except ValueError as err:
+        return jsonify({"error": str(err)}), 401
 
     except Exception as err:
         return jsonify({"error": str(err)}), 500
 
-    user = User.query.filter_by(email=email).first()
-    
-    if not(email == user.email) or not(bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8'))):
-        return jsonify({"error": "Invalid credentials"}), 401
-    
-    #-------------------------------------------------------------------------
-    #Generate token with JWT
+    #Generates JWT token for valid users
+    access_token = create_access_token(identity=user.id)
+
+    return jsonify({"access_token": access_token}), 200
