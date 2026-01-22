1fb69d5a3802e01e6dc375406609ee0342ce2e54
Bernardo-G-Cunha <bernardo.garcia.cunha@gmail.com>
2026-01-22 19:59:44 -0300

    Add update_task function.

--- backend/app/services/tasks.py
+++ backend/app/services/tasks.py
@@ -2,8 +2,8 @@ from app.extensions import db
 from app.models.user import User
 from app.models.task import Task
 from app.schemas.user_schema import *
-from app.exceptions.auth_exceptions import *
-from app.dtos.dto_task import TaskCreateDTO
+from app.exceptions import TaskNotFoundError
+from app.dtos.dto_task import TaskCreateDTO, TaskUpdateDTO
 from sqlalchemy.exc import IntegrityError, OperationalError
 
 
@@ -14,7 +14,24 @@ def create_task(task_data: TaskCreateDTO, user_id: int) -> None:
         db.session.add(new_task)
         db.session.commit()
 
-    except IntegrityError as e:
+    except IntegrityError:
         db.session.rollback()
-        raise OperationalError() from e
+        raise
 
+def update_task(update_task_data: TaskUpdateDTO, user_id: int, task_id: int) -> None:
+    
+    task = Task.query.filter_by(id=task_id, user_id=user_id).first()
+    
+    if not task:
+        raise TaskNotFoundError()
+
+    for field, value in update_task_data.__dict__.items():
+        if value is not None and field != "id":
+            setattr(task, field, value)
+
+    try:
+        db.session.commit()
+
+    except IntegrityError:
+        db.session.rollback()
+        raise
