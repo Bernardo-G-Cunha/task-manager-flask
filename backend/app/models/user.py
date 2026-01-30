bf33b6455a4077adffe827b04c6bec47b5108ef8
Bernardo-G-Cunha <bernardo.garcia.cunha@gmail.com>
2026-01-29 21:56:34 -0300

    Adjust User model adding the role column.

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -5,9 +5,10 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String, nullable = False)
-    email = db.Column(db.String, nullable = False, unique=True)
-    password = db.Column(db.String, nullable = False)
+    username = db.Column(db.String(80), nullable = False)
+    email = db.Column(db.String(120), nullable = False, unique=True)
+    password = db.Column(db.String(255), nullable = False)
+    role = db.Column(db.String(20), nullable=False, default="USER")
 
     tasks = db.relationship('Task', back_populates='user')
     tags = db.relationship('Tag', secondary=users_tags, back_populates='users')
